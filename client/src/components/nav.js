40ffe68548503d95568cdf893d6e0ce3f0c9d44f
Jay Kim <jaykim92@gmail.com>
2020-09-22 20:59:31 -0700

    update favicon and styling

---
+++ client/src/components/nav.js
@@ -0,0 +1,12 @@
+// import packages
+import React from 'react';
+
+function Nav() {
+    return(
+        <div>
+            
+        </div>
+    );
+};
+
+export default Nav;
--- client/src/pages/front.js
+++ client/src/pages/front.js
@@ -1,25 +1,55 @@
 // import packages
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
 
-// import components
+// import material-ui components
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+// import assets
+import frontImage from '../assets/front.jpg';
 
 // material-ui styling
 const useStyles = makeStyles({
     cover: {
         height: '100vh',
-        backgroundImage: `url(${"https://media.giphy.com/media/Rl5JEocdk1jOgwYVcG/giphy.gif"})`,
+        width: '100vw',
+        backgroundImage: `url(${frontImage})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat'
+    },
+    nameFont: {
+        fontFamily: "'Monoton', cursive",
+        fontSize: '36px',
+        color: 'white'
+    },
+    navFont: {
+        fontFamily: "'century-gothic', sans-serif",
+        fontSize: '18pt',
+        color: "white",
+        textAlign: "right",
+        fontWeight: "bold"
+    },
+    fullHeight: {
+        height: '100vh',
+        width: '97vw'
+    },
+    nav: {
+        color: 'white',
+
     },
 });
 
 function Front() {
     const classes = useStyles();
     return (
-    <div>
-      <div className={classes.cover}></div>
+    <div className={classes.cover}>
+        <Grid className={classes.fullHeight} container direction="column" justify="flex-end" alignItems="flex-end">
+            <Grid item xs={1} className={classes.navFont}>Stream</Grid>
+            <Grid item xs={1} className={classes.navFont}>Social</Grid>
+            <Grid item xs={1} className={classes.navFont}>Store</Grid>
+            <Grid item xs={1} className={classes.navFont}>Stills</Grid>
+        </Grid>
     </div>
   );
 }
